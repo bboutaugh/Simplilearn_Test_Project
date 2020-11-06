633e6d7c32dddd2058754b2051aef91df534ea40
bboutaugh <brad.boutaugh@gmail.com>
2020-11-05 23:37:39 -0600

    outline and some content to portal section of app

--- online-test-project/src/app/app.module.ts
+++ online-test-project/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PortalComponent } from './portal/portal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortalComponent
   ],
   imports: [
     BrowserModule,
