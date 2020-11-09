6b5e9b4f976deb478bc083792a81a422a78b72a7
bboutaugh <brad.boutaugh@gmail.com>
2020-11-08 23:51:37 -0600

    added shorthand results functionality

--- online-test-project/src/app/test/test.component.ts
+++ online-test-project/src/app/test/test.component.ts
@@ -3,6 +3,7 @@ import { Test } from '../test.model';
 import {TestService} from '../test.service';
 import {Question} from '../question.model';
 import {Option} from '../option.model';
+import { analyzeAndValidateNgModules } from '@angular/compiler';
 
 @Component({
   selector: 'app-test',
@@ -18,6 +19,9 @@ export class TestComponent implements OnInit
   
   index:any=0;
   test: Test[];
+  userAns = new Array(10).fill("x");
+  correctCount: number = 0;
+  result:string = "See your result here after hitting submit.";
  
   
     ngOnInit(): void 
@@ -54,6 +58,25 @@ export class TestComponent implements OnInit
       }
     }
 
+    logAnswer(quesID: number, optID:CharacterData):any
+    {
+      this.userAns[quesID-1] = optID;
+      console.log(this.userAns);
+    }
+
+    processResults():void
+    {
+      var i:number;
+      for(i=0; i<10; i++)
+      {
+        if(this.userAns[i] == this.test[0].questions[i].answer)
+        {
+          this.correctCount++;
+        }
+      }
+      this.result = this.correctCount + " out of 10 are correct";
+      this.correctCount=0;
+    }
    
 }
 
